95ab5d9577b76fc7cdd6fe0f68e1dbfcb2afe487
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-03-15 21:10:07 -0300

    Main e Produto atualizados

--- 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/main/Main.java
+++ 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/main/Main.java
@@ -1,10 +1,19 @@
 package br.edu.facthus.apoo.main;
 
+import br.edu.facthus.apoo.model.Produto;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		// Instancia um produto
+		Produto p = new Produto();
+		
+		// Atribui o nome
+		p.setNome("Monitor");
+		
+		// Imprime o nome
+		System.out.println(p.getNome());
 		
 	}
 
--- 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/model/Produto.java
+++ 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/model/Produto.java
@@ -5,5 +5,13 @@ public class Produto {
 	private Integer id;
 	
 	private String nome;
-
+	
+	public void setNome(String novoNome) {
+		nome = novoNome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
 }
