823b793429090a4bd37821feaedd42539fb8887c
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-03-22 19:42:07 -0300

    Acrescenta campos descrição e quantidade.

--- 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/main/Main.java
+++ 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/main/Main.java
@@ -8,23 +8,29 @@ public class Main {
 		// Instancia um produto
 		Produto p = new Produto();
 		
-		// Atribui o id e o nome
+		// Atribui os campos
 		p.setId(123);
 		p.setNome("Monitor");
+		p.setDescricao("Monitor LED 13 Polegadas");
+		p.setQuantidade(10);
 		
-		// Imprime o id e o nome
-		System.out.println(p);
+		// Imprime os campos
 		System.out.println("Id: " + p.getId());
 		System.out.println("Nome: " + p.getNome());
+		System.out.println("Descrição: " + p.getDescricao());
+		System.out.println("Quantidade: " + p.getQuantidade());
 		
 		// Instancia um 2o produto
 		Produto p2 = new Produto();
 		p2.setId(987);
 		p2.setNome("Teclado");
+		p2.setDescricao("Teclado Mecânico ABNT 2");
+		p2.setQuantidade(50);
 		
-		System.out.println(p2);
 		System.out.println("Id: " + p2.getId());
 		System.out.println("Nome: " + p2.getNome());
+		System.out.println("Descrição: " + p2.getDescricao());
+		System.out.println("Quantidade: " + p2.getQuantidade());
 		
 	}
 
--- 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/model/Produto.java
+++ 01-ControleEstoque/src/main/java/br/edu/facthus/apoo/model/Produto.java
@@ -6,20 +6,40 @@ public class Produto {
 	
 	private String nome;
 	
-	public void setNome(String novoNome) {
-		nome = novoNome;
-	}
+	private String descricao;
 	
+	private Integer quantidade;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
 	}
-	
-	public void setId(Integer novoId) {
-		id = novoId;
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-	
-	public Integer getId() {
-		return id;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
 	}
 	
 }
