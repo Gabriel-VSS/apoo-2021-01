8da96b69cbe0963eea194333f248a74674875fd4
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-03-29 21:11:17 -0300

    Acrescenta o projeto 03 Contas

---
+++ 03-Contas/src/main/java/br/edu/facthus/apoo/Main.java
@@ -0,0 +1,25 @@
+package br.edu.facthus.apoo;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Conta conta = new Conta(123, 987, "Fernando");
+		conta.imprime();
+		
+		conta.deposito(200);
+		conta.imprime();
+		
+		conta.deposito(50);
+		conta.imprime();
+		
+		System.out.println("Digite o valor do depósito:");
+		Scanner scanner = new Scanner(System.in);
+		Integer valor = scanner.nextInt();
+		conta.deposito(valor);
+		conta.imprime();
+		
+	}
+
+}
