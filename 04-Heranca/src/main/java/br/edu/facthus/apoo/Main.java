91d8f7ff2b49e1949c6cbede89ea85f1171faf20
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-05 21:24:43 -0300

    Acrescenta exemplo Quadrado/Retângulo

--- 04-Heranca/src/main/java/br/edu/facthus/apoo/Main.java
+++ 04-Heranca/src/main/java/br/edu/facthus/apoo/Main.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import java.time.Month;
 
 public class Main {
-
-	public static void main(String[] args) {
+	
+	public static void ex01() {
 		Funcionario f1 = new Funcionario("João",
 				2000,
 				LocalDate.of(2000, Month.DECEMBER, 1));
@@ -22,7 +22,21 @@ public class Main {
 				LocalDate.of(1990, Month.OCTOBER, 10),
 				500);
 		g2.imprimeRemuneracao();
+	}
+	
+	public static void ex02() {
+		Retangulo r1 = new Retangulo(4, 3);
+		r1.imprimePerimetro();
+		r1.imprimeArea();
 		
+		Quadrado q1 = new Quadrado(5);
+		q1.imprimePerimetro();
+		q1.imprimeArea();
+	}
+
+	public static void main(String[] args) {
+		// ex01();
+		ex02();
 	}
 
 }
